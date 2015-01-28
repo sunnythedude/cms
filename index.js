61b5c71e1b8f422792685f8a5f158a4d2a668faa
abhishek gyaneshwar <abhishek.gyaneshwarp@gmail.com>
2015-01-28 09:48:53 -0800

    dan's file

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-(function(){
-	var Hapi = require('hapi');
-	var httpRequest = require('request');
+var Hapi = require('hapi');
 
 // Create a server with a host and port
-var  server = new Hapi.Server();
-
-server.connection({
-	host: 'localhost',
-	port: 8000,
-});
-
-// Routes
-server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		reply("homepage");
-	}
+var server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
 });
 
 server.route({
 	method: 'GET',
-	path: '/myflickr',
-	handler: function (request, reply) {
-		var flickr = require('./share/flickr.js');
-		var credentials = require('./share/credentials.js');
-		flickr.initSettings();
-		flickr.flickrSettings.qs.user_id = credentials.flickr.user_id;
-		flickr.flickrSettings.qs.method = "flickr.people.getPhotos";
-
-		httpRequest(flickr.flickrSettings, function (err, res, body) {
-			if (!err && res.statusCode == 200) {
-				reply(flickr.generateImages(flickr.createPhotoList(body)));	
-			};
-		});
-	}
-});
-
-server.route({
-	method: 'GET',
-	path: '/{param*}',
+	path:'/{param*}',
 	handler: {
-		directory: {
-			path: 'public',
-			listing: true
-		}
-	}
+        directory: {
+            path: "public",
+            listing: true,
+            index: false
+        }
+    }
+});
+server.route({
+	method: 'GET',
+	path:'/shared/{param*}',
+	handler: {
+        directory: {
+            path: "shared",
+            listing: true,
+            index: false
+        }
+    }
 });
 
-//Start the server;
-server.start();
-console.log('server connected by port 8000');
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/flickr', 
+    handler: function (request, reply) {
+		var credentials = require('./shared/credentials.js'),
+			flickrLib = require('./shared/flickr.js'),
+			httpRequest = require('request'),
+			flickr = {
+				"url": 'https://api.flickr.com/services/rest/',
+				"qs": {
+					"method": 'flickr.photos.search',
+					"api_key": credentials.flickr.api_key,
+					"tags": 'seabus',
+					"format": 'json',
+					"nojsoncallback": 1
+				},
+				"json": true
+			};
+		httpRequest(flickr, function (error, incomingMessage, response) {
+			if (!error && incomingMessage.statusCode === 200) {
+				var photoSrc = flickrLib.createJpgPath(response.photos.photo);
+				// todo inclass: output HTML images
+				reply("<img src='hello.png'>"); // Complete browser output
+			}
+		});
+    }
+});
 
-})();
+// Start the server
+server.start(function () {
+	console.log('Server running at ' + server.info.uri);
+});
