a8b625dc1ad2d7a68bcd595161ec4e1926e48049
abhishek gyaneshwar <abhishek.gyaneshwarp@gmail.com>
2015-02-11 11:24:17 -0800

    adding twitter route

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+server.connection({
+    host: 'localhost',
+    port: 8000
 });
 
 server.route({
-	method: 'GET',
-	path:'/{param*}',
-	handler: {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
         directory: {
             path: "public",
             listing: true,
@@ -19,9 +19,9 @@ server.route({
     }
 });
 server.route({
-	method: 'GET',
-	path:'/shared/{param*}',
-	handler: {
+    method: 'GET',
+    path: '/shared/{param*}',
+    handler: {
         directory: {
             path: "shared",
             listing: true,
@@ -30,60 +30,49 @@ server.route({
     }
 });
 
-// Add the route
 server.route({
     method: 'GET',
-    path:'/flickr', 
+    path: '/twitter',
     handler: function (request, reply) {
-		var credentials = require('./shared/credentials.js'),
-			flickrLib = require('./shared/flickr.js'),
-			httpRequest = require('request'),
-			flickr = {
-				"url": 'https://api.flickr.com/services/rest/',
-				"qs": {
-					"method": 'flickr.photos.search',
-					"api_key": credentials.flickr.api_key,
-					"tags": 'seabus',
-					"format": 'json',
-					"nojsoncallback": 1
-				},
-				"json": true
-			};
-		httpRequest(flickr, function (error, incomingMessage, response) {
-			if (!error && incomingMessage.statusCode === 200) {
-				var photoSrc = flickrLib.createJpgPath(response.photos.photo);
-				// todo inclass: output HTML images
-				reply("<img src='hello.png'>"); // Complete browser output
-			}
-		});
+        console.log("Your brwoser viewed the twitter Route");
+        reply('Twit coming soon');
     }
 });
 
+// Add the route
 server.route({
     method: 'GET',
-    path:'/myflickr', 
+    path: '/flickr',
     handler: function (request, reply) {
-		var credentials = require('./shared/credentials.js'),
-			flickr = require('./shared/flickr.js'),
-			httpRequest = require('request');
-		flickr.options.qs = flickr.options.data;
-		flickr.options.qs.api_key = credentials.flickr.api_key;
-		flickr.options.qs.user_id = credentials.flickr.user_id;
-		
-		httpRequest(flickr.options, function (error, incomingMessage, response) {
-			if (!error && incomingMessage.statusCode === 200) {
-				var html = '';
-					photoSrc = flickr.createJpgPath(response.photos.photo);
-				for (var i = 0, len = photoSrc.length; i < len; i++) {
-					html += "<img src='" + photoSrc[i] + "'>";
-				}
-				reply(html); // Complete browser output
-			}
-		});
+        var credentials = require('./shared/credentials.js'),
+                flickrLib = require('./shared/flickr.js'),
+                httpRequest = require('request'),
+                flickr = {
+                    "url": 'https://api.flickr.com/services/rest/',
+                    "qs": {
+                        "method": 'flickr.photos.search',
+                        "api_key": credentials.flickr.api_key,
+                        "user_id": 'credentials.flickr.user_id',
+                        "format": 'json',
+                        "nojsoncallback": 1
+                    },
+                    "json": true
+                };
+        httpRequest(flickr, function (error, incomingMessage, response) {
+            if (!error && incomingMessage.statusCode === 200) {
+                var photoSrc = flickrLib.createJpgPath(response.photos.photo);
+                for (var i = 0, len = photoSrc.length; i < len; i++) {
+                    html += "img src='" + photoSrc[i] + "'>";
+                };
+
+                // todo inclass: output HTML images
+                reply(html); // Complete browser output
+            }
+        });
     }
 });
 
 // Start the server
 server.start(function () {
-	console.log('Server running at ' + server.info.uri);
+    console.log('Server running at ' + server.info.uri);
 });
