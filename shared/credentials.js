0b3e03bb534fc63c8c1d4bd3643d83aa85d68542
abhishek gyaneshwar <abhishek.gyaneshwarp@gmail.com>
2015-02-19 20:14:23 -0800

    credentials with twitter

--- shared/credentials.js
+++ shared/credentials.js
@@ -1,12 +1,19 @@
 var credentials = {
-    "flickr": {
-        "api_key": 'fb41eb1c5fced0b99940b76f5e4610fb',
-        "secret": 'f1fa2458296915c4',
-        "user_id": '130692053@N06'
-        
-    }
-};
+    
+"flickr": {
+"api_key": 'fb41eb1c5fced0b99940b76f5e4610fb',
+"secret": 'f1fa2458296915c4',
+"user_id": '130692053@N06'
+},
 
+"twitter": {
+"consumer_key": 'MB6YCTCd2DtH8dkQtwts8zBPM',
+"consumer_secret": 'MCkPRUCp9EFkacIucFTRnhtYFmYS3cJP6NuthEHmuqJPfc3qSK',
+"access_token": '2458303980-10ol8Rbn5bPh34Vd0qHaJO4S1qCixQcjfMSV4IE',
+"access_token_secret": 'TnGQC17ZTtexxRZi4PMPrrn4InLKU85B3VWFG0qzftgXy'
+}
+};
 if (typeof module !== "undefined"){
 module.exports = credentials;
 }
+
